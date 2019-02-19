b5c9d777166190325a7e4dbd7c5df211d8bc283d
wuxiang <2450533591@qq.com>
2019-02-19 11:30:19 +0800

    添加student到git

---
+++ student/src/com/ithiema/demo/Student.java
@@ -0,0 +1,12 @@
+package com.ithiema.demo;
+
+public class Student {
+    public static void main(String[] args) {
+
+        System.out.println("我是一名学生");
+        System.out.println("我是一名学生");
+        System.out.println("我是一名学生");
+        System.out.println("我是一名学生");
+        System.out.println("我是一名学生");
+    }
+}
